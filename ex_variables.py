3c6c35f04b85439d198f3dae94f06055f0150bc9
T-Schluter <T-Schluter@users.noreply.github.com>
2020-07-23 11:04:40 +1000

    Initial commit

---
+++ ex_variables.py
@@ -0,0 +1,33 @@
+# Week 4 Variables and User Input: Exercises
+
+# Q1
+
+# first_number = input("Enter the first number:")
+# second_number = input("Enter the second number:")
+# answer = first_number + second_number
+# print(f"{first_number} + {second_number} = {answer}")
+
+# #Q2
+
+# first_number = input("Enter the first number:")
+# second_number = input("Enter the second number:")
+# answer = first_number * second_number
+# print(f"{first_number} * {second_number} = {answer}")
+
+# #Q3
+
+# kilometers = input("Enter the number of kiometers to convert:")
+# answer = kilometers
+# print(f"kilometers" * 1000 = {answer} meters" )
+# print(f"kilometers" * 10 000 = {answer} centimeters")
+
+# kilometers = input("Enter the number of kiometers to convert:")
+# answer = kilometers
+# print(f"kilometers" * 1000 = {answer} meters" )
+# print(f"kilometers" * 10 000 = {answer} centimeters")
+
+#Q4
+
+name = input("Enter your name:")
+height = input("Enter your height in centimeters:")
+print(f"Hi, {name} you are {height} cms tall.")
