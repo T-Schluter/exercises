68606bfe5703a5d7f190ff0478e64f240343a529
T-Schluter <T-Schluter@users.noreply.github.com>
2020-07-25 09:36:37 +1000

    Completion of tasks

--- ex_variables.py
+++ ex_variables.py
@@ -1,11 +1,22 @@
 # User Input: Exercises
 
+a = 3
+b = 9
+c = -3
+d = 3.0
+e = -9
+
 # Q1
 
 # first_number = input("Enter the first number:")
 # second_number = input("Enter the second number:")
 # answer = first_number + second_number
 # print(f"{first_number} + {second_number} = {answer}")
+
+print(a + b)
+print(c + b)
+print(d + e)
+
 
 # #Q2
 
@@ -14,17 +25,16 @@
 # answer = first_number * second_number
 # print(f"{first_number} * {second_number} = {answer}")
 
+print(a * b)
+print(c * b)
+print(d * e)
+
 # #Q3
 
-# kilometers = input("Enter the number of kiometers to convert:")
-# answer = kilometers
-# print(f"kilometers" * 1000 = {answer} meters" )
-# print(f"kilometers" * 10 000 = {answer} centimeters")
-
-# kilometers = input("Enter the number of kiometers to convert:")
-# answer = kilometers
-# print(f"kilometers" * 1000 = {answer} meters" )
-# print(f"kilometers" * 10 000 = {answer} centimeters")
+km = int(input("Enter the number of kilometers to convert:"))
+m = km * 1000
+cm = km * 10000
+print(f"Your kilometer value converts to {m} meters or {cm} centimeters.")
 
 #Q4
 
