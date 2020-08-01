caab4a660f852965b95710d6e4d63221457bd837
T-Schluter <T-Schluter@users.noreply.github.com>
2020-08-01 10:03:46 +1000

    Update ex_dictionaries.py

--- ex_dictionaries.py
+++ ex_dictionaries.py
@@ -1,71 +1,38 @@
-# dictionaries are a way to structure data , keys and values. 
-# Use curly braces to tell python its a dictonary
+# Dictionaries Exercises
 
+#Q1
 
 groceries = {
-    "Baby Spinach": 2.78,
-    "Hot Chocolate": 3.70,
-    "Crackers": 2.10,
-    "Bacon": 9.00,
-    "Carrots": 0.56,
-    "Oranges": 3.08 
+    ["Baby Spinach", 2.78],
+    ["Hot Chocolate", 3.70],
+    ["Crackers", 2.10],
+    ["Bacon", 9.00],
+    ["Carrots", 0.56],
+    ["Oranges", 3.08] 
 }
-
-print(groceries)
-
 
 # Change value of existing key:
 groceries["Hot Chocolate"] = 11.10,
 groceries["Crackers"] = 4.20,
 groceries["Carrots"] = 2.24,
 groceries["Oranges"] = 6.16,
-print(groceries)
 
-# Look up items value in the string
-# for item in groceries:
-#     print(item, groceries[item])
+print(f"===== Izzy's Food Emporium ====="
+    (groceries))
+print(f(sum(grocieries.values))
+    "=================")
 
+#Q2
 
-# # Unpacking Tupple: first item becomes the key second item becomes the tupple. FYI 
-# for index, item in enumerate(some_list):
+names = {
+    "Maddy", "Bel", "Elnaz", "Gia", "Izzy", "Joy", "Katie", "Maddie", "Tash", "Nic", "Rachel", "Bec", "Bec", "Tabitha", "Teagen", "Viv", "Anna", "Catherine", "Catherine", "Debby", "Gab", "Megan", "Michelle", "Nic", "Roxy", "Samara", "Sasha", "Sophie", "Teagan", "Viv",
 
-
-# # Shortcut to printing the key and its value in for loop
-# for key, value in groceries.items():
-#     print(key, value)
-
-# print(groceries.items())
-
-# print list value within defined keys
-cohorts = {
-    "perth": ["Anna", "Viv", "Nic", "Teagen"],
-    "brisbane": ["Teagan", "Vivian", "Nic", "Joy"]
 }
 
-# print(cohorts["perth"])
+name_count = 
 
-for cohort, peeps in cohorts.items():
-    print(cohort, peeps) #or print(f"{cohort} {peeps}")
-    for peep in peeps:
-        print(f"      {peep}")
+def name_count
+    retrun f"{name}" 
 
-print()
+#Q3
 
-#nest dictonarys in dictonary with loops
-all_cohorts = {
-    2019: {
-        "perth": ["Anna", "Sarah", "Nina", "Ellie"]
-    },
-    2020: {
-        "perth": ["Anna", "Viv", "Nic", "Teagen"],
-        "brisbane": ["Teagan", "Vivian", "Nic", "Joy"]
-    }
-}
-
-#string formating with upper and lower case
-for year, cohorts in all_cohorts.items():
-    print(year)
-    for city, cohort in cohorts.items():
-        print(f"     {city.upper()}")
-        for peep in cohort:
-            print(f"      {peep.lower()}")
